556c168ce3bfb1347956483b7e7cc91e931baa68
palkovics adam <pa.adam94@gmail.com>
2022-02-18 16:28:51 +0000

    class fname changed to path

--- main.c
+++ main.c
@@ -19,26 +19,28 @@ typedef struct parsed_op {
 } parsed_op;
 
 typedef struct class {
-  char fname[64];
+  char fpath[256];
   char cname[64];
-  parsed_op prog_parsed[ROM_SZ];
+  parsed_op prog[ROM_SZ];
   int prog_end;
 } class;
 
-int lex_file(FILE *fp, parsed_op *op) {
+int lex_file(class *class) {
+  FILE *fp = fopen(class->fpath, "r");
   char *line = NULL;
   size_t len = 0;
   int prog_line = 0;
   char *token = NULL;
   parsed_op *curr = NULL;
-
+  parsed_op *next = class->prog;
+  
   while(getline(&line, &len, fp) != -1){
     token = strtok(line, " \n");
     // skip comments and empty lines
     if (token == NULL || token[0] == '/') continue;
 
-    curr = op;
-    op++;
+    curr = next;
+    next++;
     prog_line++;
     strcpy(curr->cmd, token);
     curr->arg1[0] = '\0';
@@ -50,6 +52,7 @@ int lex_file(FILE *fp, parsed_op *op) {
     if (token == NULL) continue;
     strcpy(curr->arg2, token);
   }
+  fclose(fp);
   return prog_line;
 }
 
@@ -112,15 +115,30 @@ void build_ref_table(ref *ref_table,
 int get_class_paths(class *classes, char *input) {
   int count = 0;
   int file_or_dir = is_file_or_dir(input);
+  char path[256];
   if (file_or_dir == 1) {
     count = 1;
-    strcpy(classes[0].fname, input);
+    strcpy(classes[0].fpath, input);
   } else if (file_or_dir == 2) {
+    //set last char to "/"
+    strcpy(path, input);
     DIR *d;
     struct dirent *dir;
     char ext[4] = {0};
     int string_i = 0;
+    //set last char to "/"
+    while(path[string_i] != '\0') string_i++;
+    if (path[string_i -1] != '/') {
+      path[string_i++] = '/';
+      path[string_i] = '\0';
+    }
+    string_i = 0;
     d = opendir(input);
+    if(d == NULL) {
+      fprintf(stderr, "Can't open dir %s\n", input);
+      perror("Error:");
+      exit(1);
+    }
     while((dir = readdir(d)) != NULL){
       string_i = 0;
       while(dir->d_name[string_i] != '\0') string_i++;
@@ -129,7 +147,8 @@ int get_class_paths(class *classes, char *input) {
       for(int j = 0; j < 4; j++) ext[j] = dir->d_name[string_i - 3 + j];
       // filename doesn't end with .vm
       if (strcmp(ext, ".vm") != 0) continue;
-      strcpy(classes[count].fname, dir->d_name);
+      strcpy(classes[count].fpath, path);
+      strcat(classes[count].fpath, dir->d_name);
       strcpy(classes[count].cname, dir->d_name);
       strtok(classes[count].cname, ".");
       count++;
@@ -161,7 +180,7 @@ int main(int argc, char *argv[]){
   parsed_classes = malloc(sizeof(class) * 64);
   class_count = get_class_paths(parsed_classes, input);
   for(int i = 0; i < class_count; i++){
-    printf("%-16s %s\n", parsed_classes[i].fname, parsed_classes[i].cname);
+    printf("%-16s %s\n", parsed_classes[i].fpath, parsed_classes[i].cname);
   }
   /* prog_end = lex_file(fp, prog_parsed); */
   /* build_ref_table(ref_table, &ref_table_end, prog_parsed, prog_end); */
